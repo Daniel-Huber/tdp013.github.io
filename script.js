c941eb3bd3fff78a9900fd70ac40feb407d96110
Daniel-Huber <daniel.huber@cmail.nu>
2021-08-27 13:54:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function postFunction() {
+    var text = document.getElementById('postText');
+    var textBox = document.createElement("POST");
+    textBox.setAttribute("type", "text");
+    document.getElementById('') 
+    appendChild(textBox);
+}
